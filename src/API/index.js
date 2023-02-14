282a9e4d8b4aed1cfbbcb0362786ce6e7e973e7f
unknown <umairshamoon513@gmail.com>
2023-02-14 18:56:18 +0500

    minor bug fix

--- src/API/index.js
+++ src/API/index.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL:
-    // 'https://rocket-task-server-l5s9us8fp-umairshamoon513-gmailcom.vercel.app/api/',
-    'http://localhost:5000/api/',
+    'https://rocket-task-server-l5s9us8fp-umairshamoon513-gmailcom.vercel.app/api/',
+  // 'http://localhost:5000/api/',
 })
