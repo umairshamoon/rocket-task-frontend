121ffe9108c14718d5c5c3ef503410e954ddcc19
unknown <umairshamoon513@gmail.com>
2023-02-15 12:20:11 +0500

    bug fixed

--- src/components/Rocket/View/index.js
+++ src/components/Rocket/View/index.js
@@ -1,40 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import RocketCard from './RocketCard/RocketCard'
-// import Axios from 'API'
 import Axios from 'API'
-// import Axios from 'components/src/Axios'
 import { Grid, Pagination, Stack } from '@mui/material'
 import LoaderComponent from 'components/Loader'
-import {
-  CardContent,
-  Collapse,
-  Avatar,
-  IconButton,
-  Typography,
-  CardHeader,
-  Card,
-  CardMedia,
-  CardActions,
-} from '@mui/material'
-
-import {
-  Delete,
-  Edit,
-  Favorite,
-  ExpandMore as ExpandMoreIcon,
-  ExpandMore,
-} from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 
-import { styled } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
 export default function ViewRocket() {
-  const role = localStorage.getItem('role')
   const token = localStorage.getItem('token')
   const [rockets, setRockets] = useState([])
   const [loading, setLoading] = useState(false)
   const [pageNumber, setPageNumber] = useState(1)
-  const navigate = useNavigate()
+
   console.log(pageNumber)
   const handlePageChange = (e, v) => {
     setPageNumber(v)
